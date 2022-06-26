b51c395faa1f8f652a9025ecc7e9413208a8034c
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-06-26 14:33:45 +0100

    nuevos logs para tests

--- functions/src/componentes/storage/ImagenController.ts
+++ functions/src/componentes/storage/ImagenController.ts
@@ -2,7 +2,7 @@
 import admin from "firebase-admin";
 import {storage} from "firebase-functions/v1";
 import {basename, join} from "path";
-import os from "os";
+import * as os from "os";
 import vision from "@google-cloud/vision";
 
 export const validarImagenController = (
@@ -24,23 +24,26 @@ export const validarImagenController = (
 const validarImagenHelper = (objImage: storage.ObjectMetadata) => {
   const rutaArchivo = objImage.name || "";
   const nombreArchivo = basename(rutaArchivo);
-  console.log(nombreArchivo);
-  const plid = nombreArchivo.split(".")[0];
-  console.log(plid, "es realmente el plid?");
-  const bucket = admin.storage().bucket();
+  // const plid = nombreArchivo.split(".")[0];
+  const bucket = admin.storage().bucket("nombre_bucket");
   const tmpRutaArchivo = join(os.tmpdir(), nombreArchivo);
 
   const cliente = new vision.ImageAnnotatorClient();
+  console.log("after vision es inicializado");
 
   return bucket
       .file(rutaArchivo)
       .download({
         destination: tmpRutaArchivo,
       })
-      .then(() => {
+      .then((res) => {
+        console.log(res, "respuesta del download");
+
         return cliente.safeSearchDetection(tmpRutaArchivo);
       })
       .then((resultado) => {
+        console.log(resultado, "resultado de la api");
+
         const adulto: any = resultado[0].safeSearchAnnotation?.adult;
         const violence: any = resultado[0].safeSearchAnnotation?.violence;
         const medical: any = resultado[0].safeSearchAnnotation?.medical;
